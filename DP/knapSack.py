bde6051617952477d9d6dd585ecc4fb199e530da
rohanJa <rohan.jain150300@gmail.com>
2020-09-04 03:15:24 +0530

    Added Knapsack

---
+++ DP/knapSack.py
@@ -0,0 +1,19 @@
+wieght=list(map(int,input().split()))
+val=list(map(int,input().split()))
+cap=int(input())
+
+DP = [[0 for j in range(0,cap+1)] for i in range(0,len(wieght)+1) ]
+
+for i in range(1,len(wieght)+1):    #Looping through every wieght
+    for j in range(1,cap+1):        #Uptil the capacity
+        if(wieght[i-1]<=cap):
+            DP[i][j]=max(val[i-1]+DP[i-1][j-wieght[i-1]],DP[i-1][j])
+        else:
+            DP[i][j]=DP[i-1][j]
+
+# Array output
+for i in range(1,len(wieght)+1):
+    for j in range(1,cap+1):
+        print(DP[i][j],end=' ')
+    print()
+print("Max Value of Knapsack"+str(DP[len(wieght)][cap]))
